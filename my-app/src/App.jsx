f0a0df60e82ecb6793eae85a8820c29b89dc8868
wube0965 <wubshetabate865@gmail.com>
2025-08-04 23:14:33 +0300

    Apple Website by React js
    This Apple's Website is fully done by React js

---
+++ my-app/src/App.jsx
@@ -0,0 +1,32 @@
+
+import './css/bootstrap.css'
+import './App.css'
+import Headerr from './component/Headerr/Headerr.jsx'
+import Alertt from './component/Alertt/Alertt.jsx'
+import Ipadpro from './component/Ipadpro/Ipadpro.jsx'
+import Macbook from "./component/Macbook/Macbook.jsx";
+import Iphone from "./component/Iphone/Iphone.jsx";
+import Iphonetwo from "./component/Iphonetwo/Iphonetwo.jsx";
+import Fifthsec from './component/Fifthsec/Fifthsec.jsx'
+import Sixsec from "./component/Sixsec/Sixsec.jsx";
+import Footerr from './component/Footerr/Footerr.jsx'
+
+function App() {
+ 
+
+  return (
+    <>
+      <Headerr />
+      <Alertt />
+      <Ipadpro />
+      <Macbook />
+      <Iphone />
+      <Iphonetwo/>
+      <Fifthsec />
+      <Sixsec />
+      <Footerr/>
+    </>
+  );
+}
+
+export default App
